dc3b8915e918548de2acc10f3d0f1c7d266b18dd
AbdallahRdf <abdallahradfi@gmail.com>
2025-01-19 00:15:59 +0100

    Add cache for visited urls, so if something wrong happend and the execution stopped, if the program is restrated it won't refetch previously fetched and visited urls

--- scraping_scripts/jumiaScraper.py
+++ scraping_scripts/jumiaScraper.py
@@ -10,7 +10,11 @@ from scraping_scripts.headers import headers, user_agents
 from scraping_scripts.utils import get_with_retries
 
 base_url = "https://www.jumia.ma"
+
+#Tracks categories to prevent redundant CSV headers.
 processed_categories = set()
+
+is_error = False
 
 # extracts a single product data and saves it
 def extract_and_save_data(html: str, category: str):
@@ -41,14 +45,28 @@ def extract_and_save_data(html: str, category: str):
 
 
 def scrape_data(url: str, headers: dict, category: str):
+    global is_error
+
     page_number = 1
+    visited_urls = set()
+
+    # Load previously visited URLs (if available)
+    if os.path.exists("cache/jumia_visited_urls.txt"):
+        with open("cache/jumia_visited_urls.txt", "r") as f:
+            visited_urls = set(map(lambda url: url.strip("\n"), f.readlines()))
+
     while True:
         try:
+            if url in visited_urls:
+                print(f"Skipping already visited page: {url}")
+                continue
             response = get_with_retries(url, headers=headers)
             if response.status_code != 200:
+                is_error = True
                 print(f"Error fetching {url}: Status code {response.status_code}")
                 break
-
+            
+            visited_urls.add(current_url)
             soup = BeautifulSoup(response.text, "html.parser")
             section = soup.select_one('section[class="card -fh"]')
             section = section if section else soup.select_one("[class='prd-w col8 row _2c -paxs']")
@@ -59,20 +77,30 @@ def scrape_data(url: str, headers: dict, category: str):
 
             a_tags = section.select('a')
             headers["Referer"] = url  # Update Referer header
+            
             for a_tag in a_tags:
                 a_tag_href = a_tag.get("href").strip() if a_tag.get("href") else ""
-                if a_tag_href.endswith(".html"):
-                    time.sleep(random.uniform(4, 8))
-                    headers["User-Agent"] = random.choice(user_agents)
-                    page = get_with_retries(f"{base_url}/{a_tag_href}", headers=headers)
-                    if page.status_code == 200:
-                        extract_and_save_data(html=page.text, category=category)
+                if not a_tag_href.endswith((".html", ".htm")):
+                    continue
+
+                current_url = f"{base_url}/{a_tag_href}"
+                if current_url in visited_urls:
+                    print(f"Skipping already visited page: {url}")
+                    continue
+
+                time.sleep(random.uniform(4, 8))
+                headers["User-Agent"] = random.choice(user_agents)
+                page = get_with_retries(current_url, headers=headers)
+                if page.status_code == 200:
+                    visited_urls.add(current_url)
+                    extract_and_save_data(html=page.text, category=category)
 
             # Move to the next page
             page_number += 1
             url = url.replace(f"?page={page_number-1}", f"?page={page_number}")
 
         except Exception as e:
+            is_error = True
             current_time = datetime.now()
             print(f"Error while scraping jumia, page {page_number} of {category}:")
             print(e)
@@ -80,12 +108,19 @@ def scrape_data(url: str, headers: dict, category: str):
             logging.error(e)
             break
 
+    # Save visited URL
+    with open("cache/jumia_visited_urls.txt", "a") as f:
+        f.writelines(f"{url}\n" for url in visited_urls)
+
 
 def main():
     logging.basicConfig(filename="logs/jumia_scraping_errors.log", level=logging.ERROR)
 
     if not os.path.exists("./data"):
         os.mkdir("./data")
+
+    if not os.path.exists("./cache"):
+        os.mkdir("./cache")
 
     categories = {
         "smartphones": "https://www.jumia.ma/telephones-smartphones/?page=1#catalog-listing",
@@ -104,6 +139,11 @@ def main():
         print(f"scraping data finished for category '{category}' in jumia")
 
     print("scraping data finished for jumia")
+    
+    # if no error, then the scraping finished successfully, truncate the cache/jumia_visited_urls.txt
+    if not is_error:
+        with open("cache/jumia_visited_urls.txt", "w") as f:
+            pass
 
 if __name__ == "__main__":
     main()

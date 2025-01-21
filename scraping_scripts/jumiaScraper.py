c4adb1abac93a33f2867538dff4247a75865b9dc
AbdallahRdf <abdallahradfi@gmail.com>
2025-01-21 16:22:41 +0100

    Update jumiaScraper code for better cache and prevent scraped data loss when an error occurs

--- scraping_scripts/jumiaScraper.py
+++ scraping_scripts/jumiaScraper.py
@@ -9,16 +9,8 @@ import os
 from scraping_scripts.headers import headers, user_agents
 from scraping_scripts.utils import get_with_retries
 
-base_url = "https://www.jumia.ma"
-
-#Tracks categories to prevent redundant CSV headers.
-processed_categories = set()
-
-is_error = False
-
 # extracts a single product data and saves it
-def extract_and_save_data(html: str, category: str):
-    global processed_categories
+def extract_data(html: str, category: str, data: list):
     soup = BeautifulSoup(html, "html.parser")
     title = soup.select_one("h1[class='-fs20 -pts -pbxs']")
     title = title.text.strip() if title else None
@@ -31,28 +23,35 @@ def extract_and_save_data(html: str, category: str):
         discount = soup.select_one("[class='bdg _dsct _dyn -mls']")
         discount = "discount" if discount else "no discount"
 
-        df = pd.DataFrame([{
+        data.append({
             "name": title,
             "price": price,
             "website": "jumia",
             "category": category,
             "scraping_date": datetime.now().date(),
             "promotion": discount,
-        }])
+        })
+
+
+def save_to_csv(data: list, category: str, processed_categories: set) -> None:
+    if len(data) > 0:
+        df = pd.DataFrame(data)
         header = not (category in processed_categories)
         df.to_csv(f"data/jumia_{category}.csv", index=False, mode="a", header=header)
         processed_categories.add(category) # ensure headers are only written once
+        data.clear()
 
 
-def scrape_data(url: str, headers: dict, category: str):
-    global is_error
+def scrape_data(url: str, headers: dict, category: str, processed_categories: set, data: list) -> bool:
+    base_url = "https://www.jumia.ma"
+    is_error = False
 
     page_number = 1
     visited_urls = set()
 
     # Load previously visited URLs (if available)
-    if os.path.exists("cache/jumia_visited_urls.txt"):
-        with open("cache/jumia_visited_urls.txt", "r") as f:
+    if os.path.exists(f"cache/jumia_visited_{category}_urls.txt"):
+        with open(f"cache/jumia_visited_{category}_urls.txt", "r") as f:
             visited_urls = set(map(lambda url: url.strip("\n"), f.readlines()))
 
     while True:
@@ -81,7 +80,7 @@ def scrape_data(url: str, headers: dict, category: str):
 
                 current_url = f"{base_url}/{a_tag_href}"
                 if current_url in visited_urls:
-                    print(f"Skipping already visited page: {url}")
+                    print(f"Skipping already visited page: {current_url}")
                     continue
 
                 time.sleep(random.uniform(4, 8))
@@ -89,13 +88,16 @@ def scrape_data(url: str, headers: dict, category: str):
                 page = get_with_retries(current_url, headers=headers)
                 if page.status_code == 200:
                     visited_urls.add(current_url)
-                    extract_and_save_data(html=page.text, category=category)
+                    extract_data(html=page.text, category=category, data=data)
+
+            save_to_csv(data, category, processed_categories)
 
             # Move to the next page
             page_number += 1
             url = url.replace(f"?page={page_number-1}", f"?page={page_number}")
 
         except Exception as e:
+            save_to_csv(data, category, processed_categories)
             is_error = True
             current_time = datetime.now()
             print(f"Error while scraping jumia, page {page_number} of {category}:")
@@ -103,13 +105,21 @@ def scrape_data(url: str, headers: dict, category: str):
             logging.error(f"{current_time} : Error on page {page_number} of {category}: {url}:")
             logging.error(e)
             break
-
-    # Save visited URL
-    with open("cache/jumia_visited_urls.txt", "a") as f:
-        f.writelines(f"{url}\n" for url in visited_urls)
+        
+    if is_error:
+        # Save visited URL
+        with open(f"cache/jumia_visited_{category}_urls.txt", "a") as f:
+            f.writelines(f"{url}\n" for url in visited_urls)
+            
+    return is_error
 
 
 def main():
+    #Tracks categories to prevent redundant CSV headers.
+    processed_categories = set()
+
+    data = []   
+
     logging.basicConfig(filename="logs/jumia_scraping_errors.log", level=logging.ERROR)
 
     categories = {
@@ -119,21 +129,27 @@ def main():
     }
 
     for category, url in categories.items():
+        print(f"scraping data started for category '{category}' in jumia")
+
         if os.path.exists(f"data/jumia_{category}.csv"): # if file exists:
             with open(f"data/jumia_{category}.csv", mode="r") as file:
                 if file.read(1): # if file is not empty
                     # ensure that the headers are not written to the csv file as it is not empty
                     processed_categories.add(category)
 
-        scrape_data(url, headers=headers, category=category)
-        print(f"scraping data finished for category '{category}' in jumia")
+        error = scrape_data(url, headers=headers, category=category, processed_categories=processed_categories, data=data)
+
+        # if no error, then the scraping finished successfully, truncate the cache/jumia_visited_urls.txt
+        if not error:
+            if os.path.exists(f"cache/jumia_visited_{category}_urls.txt"):
+                with open(f"cache/jumia_visited_{category}_urls.txt", "w") as f:
+                    pass
+            print(f"scraping data finished for category '{category}' in jumia")
+        else:
+            print(f"something went worng when scraping data for category '{category}' in jumia, please check log and cache files")
 
     print("scraping data finished for jumia")
-    
-    # if no error, then the scraping finished successfully, truncate the cache/jumia_visited_urls.txt
-    if not is_error:
-        with open("cache/jumia_visited_urls.txt", "w") as f:
-            pass
+
 
 if __name__ == "__main__":
     if not os.path.exists("./data"):
